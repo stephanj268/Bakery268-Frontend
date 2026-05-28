29e61273f6d1fee340eb9ee03f22612e212192cc
stephanj268 <stephanofficial98@gmail.com>
2026-05-27 22:08:34 -0400

    jbk

---
+++ src/app/Shared/navigation/navigation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.scss',
+})
+export class Navigation {}
