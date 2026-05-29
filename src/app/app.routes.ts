4e913b855882eca6a3dd835425bbe94ca024d4e8
LUHCaprisun <travoughna1@gmail.com>
2026-05-28 21:26:46 -0400

    sad as

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './Shared/form/form';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: App},
+    { path: 'form', component: FormComponent },
+];
